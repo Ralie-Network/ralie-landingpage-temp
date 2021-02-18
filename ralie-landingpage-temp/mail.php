173a6827a694c5a3e77243e917606e1627d5f420
menezes <78161484+menezesphill@users.noreply.github.com>
2021-02-18 05:50:36 -0300

    bug fix on mail form

--- ralie-landingpage-temp/mail.php
+++ ralie-landingpage-temp/mail.php
@@ -1,5 +1,5 @@
 <?php 
-if(isset($_POST['submit'])){
+
     $to = "contact@ralie.io"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
     $first_name = $_POST['name'];
@@ -15,5 +15,5 @@ if(isset($_POST['submit'])){
     mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
     header("Location: {$_SERVER['HTTP_REFERER']}");
     exit;
-    }
+    
 ?>
